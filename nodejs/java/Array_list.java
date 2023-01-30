ff38ed77d38eaad874d35fe838f9ab5554500cb0
pythonncoder <thirupathinaveen346@gmail.com>
2023-01-30 14:41:56 +0530

    'ok'

---
+++ nodejs/java/Array_list.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.*;
+
+public class Array_list {
+
+	public static void main(String[] args) {
+		List  li = new arraylist();
+		
+		System.out.println(li);
+
+	}
+
+}
